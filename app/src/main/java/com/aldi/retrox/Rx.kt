b5288b0749a679c35396cc3cff035801181c6b22
ganiaaldi <mrgad98@gmail.com>
2019-11-01 18:38:50 +0700

    Create Rx Class & Add Button

---
+++ app/src/main/java/com/aldi/retrox/Rx.kt
@@ -0,0 +1,12 @@
+package com.aldi.retrox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Rx : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rx)
+    }
+}
